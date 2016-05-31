2aeb4619c1e40a8e15495a18df84a304104c01d7
Yang Lin <todoubaba@gmail.com>
2016-06-01 02:26:03 +0800

    修正实践学分提示错误 (#22)

--- src/plan/scheme/draft/item/draft-item.model.ts
+++ src/plan/scheme/draft/item/draft-item.model.ts
@@ -29,7 +29,7 @@ Scheme.prototype.checkCredit = function() {
 
         directions.forEach(d => {
             if (d.practiceCredit / d.total < that.practiceCreditRatio) {
-                errors.push(`${d.name.replace('方向', '')}方向的专业必修课比例小于${that.practiceCreditRatio * 100}%（${d.practiceCredit}/${d.total}）`);
+                errors.push(`${d.name.replace('方向', '')}方向的专业必修课中实践学分的比例小于${that.practiceCreditRatio * 100}%（${d.practiceCredit}/${d.total}）`);
             }
         });
     } else {
@@ -37,7 +37,7 @@ Scheme.prototype.checkCredit = function() {
         let practiceCredit = compulsories.reduce((sum, s) => sum += s.practiceCredit, 0);
 
         if (practiceCredit / total < that.practiceCreditRatio) {
-            errors.push(`专业必修课比例小于${that.practiceCreditRatio * 100}%（${practiceCredit}/${total}）`);
+            errors.push(`专业必修课中实践学分的比例小于${that.practiceCreditRatio * 100}%（${practiceCredit}/${total}）`);
         }
     }
 
@@ -46,7 +46,7 @@ Scheme.prototype.checkCredit = function() {
     let electivePracticeCredit = electives.reduce((sum, s) => sum += s.electivePracticeCredit, 0);
 
     if (electivePracticeCredit / electiveCredit < that.practiceCreditRatio) {
-        errors.push(`专业选课课比例小于${that.practiceCreditRatio * 100}%（${electivePracticeCredit}/${electiveCredit}）`);
+        errors.push(`专业选修课中实践学分的比例小于${that.practiceCreditRatio * 100}%（${electivePracticeCredit}/${electiveCredit}）`);
     }
 
     return errors;
