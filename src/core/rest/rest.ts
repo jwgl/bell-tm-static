9218c08dea4250fb46aacba00a37c914fdd00adc
Yang Lin <todoubaba@gmail.com>
2016-10-20 23:49:10 +0800

    Angular已提供XSRF支持 (#55)

--- src/core/rest/rest.ts
+++ src/core/rest/rest.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 export {Observable};
@@ -9,45 +9,22 @@ export class Rest {
     constructor(private http: Http) {}
 
     get(url: string): Observable<any> {
-        let headers = new Headers({Accept: 'application/json'});
-        return this.http.get(url, {headers: headers})
-        .map(res => res.json());
+        return this.http.get(url).map(res => res.json());
     }
 
     post(url: string, data: Object): Observable<any> {
-        return this.http.post(url, JSON.stringify(data), {headers: this.getPostHeader()})
-        .map(res => res.json());
+        return this.http.post(url, JSON.stringify(data)).map(res => res.json());
     }
 
     put(url: string, data: Object): Observable<any> {
-        return this.http.put(url, JSON.stringify(data), {headers: this.getPostHeader()})
-        .map(res => res.json());
+        return this.http.put(url, JSON.stringify(data)).map(res => res.json());
     }
 
     patch(url: string, data: Object): Observable<any> {
-        return this.http.patch(url, JSON.stringify(data), {headers: this.getPostHeader()})
-        .map(res => res.json());
+        return this.http.patch(url, JSON.stringify(data)).map(res => res.json());
     }
 
     delete(url: string): Observable<any> {
-        return this.http.delete(url, {headers: this.getPostHeader()})
-        .map(res => res.json());
-    }
-
-    private getPostHeader() {
-        return new Headers({
-            Accept: 'application/json',
-            'X-XSRF-TOKEN': this.getCsrfCookie(),
-        });
-    }
-
-    private getCsrfCookie(): string {
-        let value = '; ' + document.cookie;
-        let parts = value.split('; XSRF-TOKEN=');
-        if (parts.length === 2) {
-            return parts.pop().split(';').shift();
-        } else {
-            return undefined;
-        }
+        return this.http.delete(url).map(res => res.json());
     }
 }
