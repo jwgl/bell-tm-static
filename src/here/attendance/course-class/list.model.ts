8e0400381a5dc2f2f169a6f570884897fd2a270d
Yang Lin <todoubaba@gmail.com>
2017-05-31 02:09:37 +0800

    Fix disqualify button (#235)

--- src/here/attendance/course-class/list.model.ts
+++ src/here/attendance/course-class/list.model.ts
@@ -6,26 +6,41 @@ declare module '../shared/attendance.model' {
         disqualified: boolean;
         rollcallOverflow: boolean;
         leaveOverflow: boolean;
-        showDisqual: boolean;
         showDetails: boolean;
-
-        getDisqualButtonClass(): string;
+        showDisqual: boolean;
+        disqualifyButtonClass: string;
     }
 }
 
-Student.prototype.getDisqualButtonClass = function(this: Student) {
-    if (this.showDetails) {
-        if (this.disqualified) {
-            if (this.rollcallOverflow || this.leaveOverflow) {
-                return 'btn-secondary';
+Object.defineProperty(Student.prototype, 'showDisqual', {
+    /* tslint:disable:object-literal-shorthand*/
+    get: function(this: Student) {
+        return this.disqualified || this.rollcallOverflow || this.leaveOverflow;
+    },
+    /* tslint:enable:object-literal-shorthand*/
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Student.prototype, 'disqualifyButtonClass', {
+    /* tslint:disable:object-literal-shorthand*/
+    get: function(this: Student) {
+        if (this.showDetails) {
+            if (this.disqualified) {
+                if (this.rollcallOverflow || this.leaveOverflow) {
+                    return 'btn-secondary';
+                } else {
+                    return 'btn-success';
+                }
             } else {
-                return 'btn-success';
+                return 'btn-danger';
             }
-        } else {
-            return 'btn-danger';
         }
-    }
-};
+    },
+    /* tslint:enable:object-literal-shorthand*/
+    enumerable: true,
+    configurable: true,
+});
 
 export class CourseClass {
     id: string;
@@ -41,8 +56,6 @@ export class CourseClass {
     rollcallDisqualRatio: number;
     leaveDisqualRatio: number;
 
-    private studentMap: {[key: string]: Student} = {};
-
     constructor(dto: any) {
         this.id = dto.id;
         this.code = dto.code;
@@ -56,20 +69,20 @@ export class CourseClass {
         this.rollcallDisqualRatio = dto.rollcallDisqualRatio;
         this.leaveDisqualRatio = dto.leaveDisqualRatio;
 
+        const studentMap: {[key: string]: Student} = {};
         this.students = dto.students.map((s: any) => {
             const student = new Student(s);
             student.subject = s.subject;
             student.disqualified = s.disqualified;
             student.rollcallOverflow = false;
             student.leaveOverflow = false;
-            student.showDisqual = false;
             student.showDetails = false;
-            this.studentMap[student.id] = student;
+            studentMap[student.id] = student;
             return student;
         });
 
         dto.attendances.forEach((a: any) => {
-            const student = this.studentMap[a.id];
+            const student = studentMap[a.id];
             if (!student) {
                 return;
             }
@@ -82,8 +95,6 @@ export class CourseClass {
 
             student.rollcallOverflow = student.total > this.totalSection / this.rollcallDisqualRatio;
             student.leaveOverflow = student.leave > this.totalSection / this.leaveDisqualRatio;
-            student.showDisqual = student.disqualified || student.rollcallOverflow || student.leaveOverflow;
-
             student.showDetails = true;
         });
     }
