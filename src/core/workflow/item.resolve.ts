f35b51ca42314a0a6998fde20f7476918ff3f0b0
Yang Lin <todoubaba@gmail.com>
2018-03-17 02:19:47 +0800

    Fix workflow approval next and prev button error (#289)

--- src/core/workflow/item.resolve.ts
+++ src/core/workflow/item.resolve.ts
@@ -13,9 +13,17 @@ export class WorkflowItemResolve implements Resolve<any> {
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
         const type = route.params['type'];
         const id = route.params['id'];
-        const wi = route.params['wi'];
         const query = route.params['query'];
 
-        return this.workflow.loadItem(type, id, wi, query);
+        // Fix next and prev button error when approval. Redirection should be used, but
+        // Angualr does not support passing segment param to query string param.
+        // See https://github.com/angular/angular/issues/20297
+        // redirect "/:type/:id/workitems/:wi" to "/:type/:id?wi=:wi"
+        if (route.params['wi']) {
+            this.router.navigate([type, id], {queryParams: {wi: route.params['wi']}});
+        } else {
+            const wi = route.queryParams['wi'];
+            return this.workflow.loadItem(type, id, wi, query);
+        }
     }
 }
